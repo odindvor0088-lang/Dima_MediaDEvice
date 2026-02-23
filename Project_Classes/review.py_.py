775e7c310dcadb439b7bf5b9d5d77d12a84a00ca
dIMON <odindvor0088@gmail.com>
2026-02-23 12:25:10 +0300

    создал класс Review с приватными атрибутами

---
+++ Project_Classes/review.py_.py
@@ -0,0 +1,8 @@
+class Review:
+    def __init__(self, title, content, author, date, pros, cons):
+        self.__title = title
+        self.__content = content
+        self.__author = author
+        self.__date = date
+        self.__pros = pros
+        self.__cons = cons
