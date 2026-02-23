363f2c2bd4d11894d8f46fb01fd74cbe43917caf
dIMON <odindvor0088@gmail.com>
2026-02-23 12:39:20 +0300

    Реализовал свойство property для атрибута content

--- Project_Classes/review.py_.py
+++ Project_Classes/review.py_.py
@@ -20,6 +20,19 @@ class Review:
             print(f'Заголовок не может быть написан числами!')
 
 
+    @property
+    def content(self):
+        return self.__content
+
+    @content.setter
+    def content(self, value):
+        if isinstance(value, str):
+            self.__content = value
+            print(f'Текст вашего обзора(Проверьте, всё ли вы правильно указали.): {self.__content}')
+        else:
+            print(f'Обзор не может состоять только из чисел!')
+
+
 
 
 
