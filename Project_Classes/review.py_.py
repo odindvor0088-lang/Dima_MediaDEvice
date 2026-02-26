3ab1e7972fe442a82bed73647a76ad46c4633415
dIMON <odindvor0088@gmail.com>
2026-02-26 18:09:07 +0300

    Сделал рефакторинг класса Review

--- Project_Classes/review.py_.py
+++ Project_Classes/review.py_.py
@@ -1,4 +1,14 @@
 class Review:
+    """
+      Класс для представления обзора
+      Атрибуты:
+          title (str): заголовок обзора
+          content (str): текст обзора
+          author (str): автор обзора
+          date (str): дата создания обзора в формате
+          pros (list): список плюсов
+          cons (list): список минусов
+      """
     def __init__(self, title, content, author = "Эксперт", date = None, pros = None, cons = None):
         self.__title = title
         self.__content = content
@@ -7,12 +17,19 @@ class Review:
         self.__pros = pros.copy() if pros is not None else []
         self.__cons = pros.copy() if cons is not None else []
 
+
+
     @property
-    def title(self):
+    def title(self) -> str:
+        """Геттер для заголовка. Возвращает текущий заголовок обзора."""
         return self.__title
 
+
     @title.setter
-    def title(self, value):
+    def title(self, value: str) -> None:
+        """Сеттер для заголовка. Принимает новый заголовок, проверяет его на то, чтобы он был строкой.
+               И выводит сообщение если заголовок неправильный.
+               """
         if isinstance(value, str):
             self.__title = value
             print(f'Заголовок вашего обзора: {self.__title}')
@@ -20,12 +37,18 @@ class Review:
             print(f'Заголовок не может быть написан числами!')
 
 
+
     @property
-    def content(self):
+    def content(self) -> str:
+        """Геттер для содержания. Возвращает текст обзора."""
         return self.__content
 
+
     @content.setter
-    def content(self, value):
+    def content(self, value: str) -> None:
+        """ Сеттер для содержания. Принимает новый текст обзора и проверяет его с помощью isinstanse.
+              И выводит сообщение если обзор неправильный.
+              """
         if isinstance(value, str):
             self.__content = value
             print(f'Текст вашего обзора(Проверьте, всё ли вы правильно указали.): {self.__content}')
@@ -33,36 +56,61 @@ class Review:
             print(f'Обзор не может состоять только из чисел!')
 
 
+
     @property
-    def author(self):
+    def author(self) -> str:
+        """Геттер для автора. Возвращает имя автора обзора."""
         return self.__author
 
+
     @author.setter
-    def author(self, value):
+    def author(self, value: str) -> None:
+        """ Сеттер для автора. Сохраняет имя автора."""
         self.__author = value
         print(f'Другие пользователи будут видеть вас под ником: {self.__author}')
 
 
+
     @property
-    def date(self):
+    def date(self) -> str:
+        """Геттер для даты. Возвращает дату создания обзора."""
         return self.__date
 
+
     @date.setter
-    def date(self, value):
+    def date(self, value: str) -> None:
         self.__date = value
 
 
+
     @property
-    def pros(self):
+    def pros(self) -> list:
+        """ Геттер для списка плюсов. Возвращает КОПИЮ списка"""
         return self.__pros.copy()
 
 
+    @pros.setter
+    def pros(self, list_pros: list) -> None:
+        """Сохраняется копия списка для инкапсуляции."""
+        self.pros = list_pros[:]
+
+
+
     @property
-    def cons(self):
+    def cons(self) -> list:
+        """Сохраняется копия списка для инкапсуляции."""
         return self.__cons.copy()
 
 
-    def add_pro(self, pro_text):
+    @cons.setter
+    def cons(self, list_cons: list) -> None:
+        """Сохраняется копия списка для инкапсуляции."""
+        self.__cons = list_cons[:]
+
+
+
+    def add_pro(self, pro_text: str) -> None:
+        """Добавляет новый плюс в список."""
         if len(pro_text) > 200:
             print(f'Текст плюса слишком длинный')
         else:
@@ -71,7 +119,8 @@ class Review:
 
 
 
-    def add_con(self, con_text):
+    def add_con(self, con_text: str) -> None:
+        """Добавляет новый минус в список."""
         if len(con_text) > 200:
             print(f'Текст минуса слишком длинный')
         else:
@@ -79,7 +128,9 @@ class Review:
             print(f'Ваш минус добавлен✅')
 
 
-    def remove_pro(self, index):
+
+    def remove_pro(self, index: int) -> None:
+        """Удаляет плюс по индексу."""
         if 0 <= index < len(self.__pros):
             removed = self.__pros.pop(index)
             print(f"Плюс '{removed}' удалён")
@@ -87,12 +138,15 @@ class Review:
             print(f'Индекс {index} вне диапазона')
 
 
-    def remove_con(self, index):
+
+    def remove_con(self, index: int) -> None:
+        """Удаляет минус по индексу."""
         if 0 <= index < len(self.__cons):
             removed = self.__cons.pop(index)
             print(f"Минус '{removed}' удалён")
         else:
             print(f'Индекс {index} вне диапазона')
+
 
 
 if __name__ == "__main__":
