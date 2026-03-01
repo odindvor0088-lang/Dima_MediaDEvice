cc903d8f9d775cf6f17b706b5c772829629a3783
Павел Муслаев <92016943+PavelMuslaev@users.noreply.github.com>
2026-03-01 18:16:28 +0300

    Решение задачи https://github.com/odindvor0088-lang/Dima_MediaDEvice/issues/6
    Оформление документации и аннотации класса Review
    
    ---------
    
    Co-authored-by: dIMON <odindvor0088@gmail.com>

--- Project_Classes/review.py_.py
+++ Project_Classes/review.py_.py
@@ -1,99 +1,140 @@
+from datetime import datetime
+
+
 class Review:
-    def __init__(self, title, content, author = "Эксперт", date = None, pros = None, cons = None):
-        self.__title = title
-        self.__content = content
-        self.__author = author
-        self.__date = date
-        self.__pros = pros.copy() if pros is not None else []
-        self.__cons = pros.copy() if cons is not None else []
+    """
+      Класс для представления обзора
+      """
+    def __init__(self, title: str, content: str, author: str = "Эксперт",
+                 date: datetime = None, pros: list[str] = None, cons: list[str] = None) -> None:
+        """
+        Инициализация объекта Review.
+
+            title: Заголовок обзора
+            content: Текст обзора
+            author: Автор обзора (по умолчанию "Эксперт")
+            date: Дата создания
+            pros: Список плюсов
+            cons: Список минусов
+        """
+        self.title = title
+        self.content = content
+        self.author = author
+        self.date = datetime.now() if date is None else date
+        self.pros = pros.copy() if pros is not None else []
+        self.cons = cons.copy() if cons is not None else []
 
     @property
-    def title(self):
+    def title(self) -> str:
+        """Возвращает текущий заголовок обзора."""
         return self.__title
 
     @title.setter
-    def title(self, value):
+    def title(self, value: str) -> None:
+        """
+            Принимает новый заголовок, проверяет его на то, чтобы он был строкой.
+            И выводит сообщение если заголовок неправильный.
+        """
         if isinstance(value, str):
             self.__title = value
             print(f'Заголовок вашего обзора: {self.__title}')
         else:
             print(f'Заголовок не может быть написан числами!')
 
-
     @property
-    def content(self):
+    def content(self) -> str:
+        """Возвращает текст обзора."""
         return self.__content
 
     @content.setter
-    def content(self, value):
+    def content(self, value: str) -> None:
+        """
+            Принимает новый текст обзора и проверяет его с помощью isinstanse.
+            И выводит сообщение если обзор неправильный.
+        """
         if isinstance(value, str):
             self.__content = value
             print(f'Текст вашего обзора(Проверьте, всё ли вы правильно указали.): {self.__content}')
         else:
             print(f'Обзор не может состоять только из чисел!')
 
-
     @property
-    def author(self):
+    def author(self) -> str:
+        """Возвращает имя автора обзора."""
         return self.__author
 
     @author.setter
-    def author(self, value):
+    def author(self, value: str) -> None:
+        """Сохраняет имя автора."""
         self.__author = value
         print(f'Другие пользователи будут видеть вас под ником: {self.__author}')
 
-
     @property
-    def date(self):
+    def date(self) -> datetime:
+        """Возвращает дату создания обзора."""
         return self.__date
 
     @date.setter
-    def date(self, value):
-        self.__date = value
-
+    def date(self, new_date: datetime) -> None:
+        self.__date = new_date
 
     @property
-    def pros(self):
+    def pros(self) -> list[str]:
+        """Возвращает КОПИЮ списка"""
         return self.__pros.copy()
 
+    @pros.setter
+    def pros(self, list_pros: list[str]) -> None:
+        """Сохраняется копия списка для инкапсуляции."""
+        self.__pros = list_pros.copy()
 
     @property
-    def cons(self):
+    def cons(self) -> list:
+        """Сохраняется копия списка для инкапсуляции."""
         return self.__cons.copy()
 
+    @cons.setter
+    def cons(self, list_cons: list) -> None:
+        """Сохраняется копия списка для инкапсуляции."""
+        self.__cons = list_cons.copy()
 
-    def add_pro(self, pro_text):
+    def add_pro(self, pro_text: str) -> None:
+        """
+        Добавляет новый плюс в список.
+        :param pro_text: текст плюса
+        """
         if len(pro_text) > 200:
             print(f'Текст плюса слишком длинный')
         else:
             self.__pros.append(pro_text)
             print(f'Ваш плюс добавлен✅')
 
-
-
-    def add_con(self, con_text):
+    def add_con(self, con_text: str) -> None:
+        """
+        Добавляет новый минус в список.
+        :param con_text: текст минуса
+        """
         if len(con_text) > 200:
             print(f'Текст минуса слишком длинный')
         else:
             self.__cons.append(con_text)
             print(f'Ваш минус добавлен✅')
 
-
-    def remove_pro(self, index):
-        if 0 <= index < len(self.__pros):
+    def remove_pro(self, index: int) -> None:
+        """Удаляет плюс по индексу."""
+        if -len(self.__pros) <= index < len(self.__pros):
             removed = self.__pros.pop(index)
             print(f"Плюс '{removed}' удалён")
         else:
             print(f'Индекс {index} вне диапазона')
 
-
-    def remove_con(self, index):
-        if 0 <= index < len(self.__cons):
+    def remove_con(self, index: int) -> None:
+        """Удаляет минус по индексу."""
+        if -len(self.__cons) <= index < len(self.__cons):
             removed = self.__cons.pop(index)
             print(f"Минус '{removed}' удалён")
         else:
             print(f'Индекс {index} вне диапазона')
-
 
 if __name__ == "__main__":
     # 1. ТЕСТ СОЗДАНИЯ ОБЗОРА
