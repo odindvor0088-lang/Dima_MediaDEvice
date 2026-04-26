b9981d36d76800a45bba827521d2cfb73c559546
Павел Муслаев <muslaevpavel@gmail.com>
2026-04-26 14:32:31 +0300

    Changes by Дмитрий

--- src/device/allowed_categories.py
+++ src/device/allowed_categories.py
@@ -3,13 +3,19 @@ from enum import StrEnum
 
 class AllowedCategories(StrEnum):
     """
-        Перечисление разрешённых категорий устройств.
+    Перечисление разрешённых категорий устройств.
 
-        Используется для валидации категории при создании экземпляра класса Device.
-        Каждая категория представлена в виде строки в верхнем регистре.
+    Используется для валидации категории при создании экземпляра класса Device.
+    Каждая категория представлена в виде строки в верхнем регистре.
     """
+
     SMARTPHONE = "SMARTPHONE"
     HEADPHONE = "HEADPHONE"
     LAPTOP = "LAPTOP"
     TABLET = "TABLET"
     SMARTWATCH = "SMARTWATCH"
+
+    @classmethod
+    def to_list(cls) -> list[str]:
+        """ """
+        return [str(i) for i in cls]
